5a0be17ca4b504f90cc068978ee7a264d37896bd
MA MU N <mamunlevel225522@gmail.com>
2026-03-18 05:33:19 +0000

    something add about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 122446;
+let accountEmail = "mamun@google.com"
+var  accountPassword = "225522"
+accountCity = "Sirajganj"
+
+//account id = 2 //not allowed
+accountEmail = "mbb@gmail.com"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
