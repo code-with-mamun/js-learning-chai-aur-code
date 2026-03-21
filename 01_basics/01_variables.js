0827e5db7f3aaf1fbea97a93ef3ec8880455ae05
MA MU N <mamunlevel225522@gmail.com>
2026-03-21 15:23:13 +0000

    Variables and console table practice

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,5 +4,12 @@ var  accountPassword = "225522"
 accountCity = "Sirajganj"
 
 //account id = 2 //not allowed
+
 accountEmail = "mbb@gmail.com"
+accountPassword = "335533"
+accountCity = "Ullapara"
 console.table([accountId,accountEmail,accountPassword,accountCity]);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+/*
